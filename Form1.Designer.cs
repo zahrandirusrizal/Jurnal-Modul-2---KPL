9c005115dd1d1e4e3472bd459f474d904533b3e8
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-03-05 10:40:04 +0700

    JurnalMod2 Selesai

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -70,6 +70,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "=";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -99,6 +100,7 @@
             this.button5.TabIndex = 5;
             this.button5.Text = "2";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -108,6 +110,7 @@
             this.button6.TabIndex = 6;
             this.button6.Text = "3";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -117,6 +120,7 @@
             this.button7.TabIndex = 7;
             this.button7.Text = "4";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -126,6 +130,7 @@
             this.button8.TabIndex = 8;
             this.button8.Text = "5";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -135,6 +140,7 @@
             this.button9.TabIndex = 9;
             this.button9.Text = "6";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -154,6 +160,7 @@
             this.button11.TabIndex = 11;
             this.button11.Text = "8";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
--- Form1.cs
+++ Form1.cs
@@ -2,25 +2,50 @@ namespace PraktikumModul2
 {
     public partial class Form1 : Form
     {
+        int masukan_pertama = 0;
+        int masukan_kedua = 0;
+        int total = 0;
         public Form1()
         {
+
             InitializeComponent();
-            int total = 0;
+            
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-
+            if (masukan_pertama == 0)
+            {
+                masukan_pertama = 7;
+            }
+            else
+            {
+                masukan_kedua = 7;
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            
+            if(masukan_pertama == 0) //UNIK ANGKA 0
+            {
+                masukan_pertama = 0;
+            }
+            else
+            {
+                masukan_kedua = 0;
+            }
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-
+            if (masukan_pertama == 0)
+            {
+                masukan_pertama = 9;
+            }
+            else
+            {
+                masukan_kedua = 9;
+            }
         }
 
         private void button1_MouseClick(object sender, MouseEventArgs e)
@@ -35,7 +60,96 @@ namespace PraktikumModul2
 
         private void button4_Click(object sender, EventArgs e)
         {
-            total = total + 1;
+            if (masukan_pertama == 0)
+            {
+                masukan_pertama = 1;
+            }
+            else
+            {
+                masukan_kedua = 1;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (masukan_pertama == 0)
+            {
+                masukan_pertama = 2;
+            }
+            else
+            {
+                masukan_kedua = 2;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (masukan_pertama == 0)
+            {
+                masukan_pertama = 3;
+            }
+            else
+            {
+                masukan_kedua = 3;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (masukan_pertama == 0)
+            {
+                masukan_pertama = 4;
+            }
+            else
+            {
+                masukan_kedua = 4;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (masukan_pertama == 0)
+            {
+                masukan_pertama = 5;
+            }
+            else
+            {
+                masukan_kedua = 5;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (masukan_pertama == 0)
+            {
+                masukan_pertama = 6;
+            }
+            else
+            {
+                masukan_kedua = 6;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (masukan_pertama == 0)
+            {
+                masukan_pertama = 8;
+            }
+            else
+            {
+                masukan_kedua = 8;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            total = total + masukan_pertama + masukan_kedua;
+
+            this.label1.Text = Convert.ToString(total);
+            total = 0;
+            masukan_pertama = 0;
+            masukan_kedua = 0;
         }
     }
 }
